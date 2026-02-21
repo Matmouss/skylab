b3bc67ff7022a45f98d6d6c6af269da6ddc7d3af
Matmouss <monteuxmatthieu2@gmail.com>
2026-02-21 19:01:24 +0100

    creation graphics.py

--- code/topography.py
+++ code/topography.py
@@ -3,13 +3,6 @@ import numpy as np
 from pyproj import Transformer
 import os
 
-#visuel
-import shapely
-from shapely import geometry
-from shapely.geometry import shape, Point, LineString, Polygon
-from rasterio.plot import show
-import matplotlib 
-import matplotlib.pyplot as plt
 
 
 """
@@ -64,19 +57,6 @@ class Map:
 
     def __del__(self):
         self.dataset.close()
-
-    def dataset_plot(dataset):
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-        full_img = dataset.read()
-        nir=dataset.read(dataset.count)
-
-        ax1.imshow(nir, cmap='gray')
-        ax1.set_title('gray -Band 1')
-
-        ax2.imshow(full_img[0, :, :])
-        ax2.set_title('color - Band 1 '.format(nir.shape))
-        plt.show()
 
     def get_elevation(self, coord_a, coord_b):
         """
